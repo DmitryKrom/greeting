7ad71b84cc6bd3f3ba1f9ba5372173ba8ccada7d
DmitryKrom <krom77@hotmail.de>
2021-07-13 19:15:27 +0200

    modified returned value

--- greeting.go
+++ greeting.go
@@ -3,7 +3,7 @@ package greeting
 import "fmt"
 
 func Hello(name string) string {
-	massage := fmt.Sprintf("Hi, %v, how are you doing?", name)
+	m := fmt.Sprintf("Hi, %v, how are you doing?", name)
 
-	return massage
+	return m
 }
