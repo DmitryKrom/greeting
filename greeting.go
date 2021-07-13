8613ed43cd71f5604398b34d3c4a4aa0340f5044
DmitryKrom <krom77@hotmail.de>
2021-07-13 23:01:05 +0200

    random sentence

--- greeting.go
+++ greeting.go
@@ -3,13 +3,31 @@ package greeting
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("no name given")
 	}
-	message := fmt.Sprintf("Hi, %v, how are you doing?", name)
+	message := fmt.Sprintf(sentenceToChoose(), name)
 
 	return message, nil
+}
+
+func init (){
+	rand.Seed(time.Now().UnixNano())
+	rand.Seed(1)
+}
+
+func sentenceToChoose()string{
+	sentences := []string{
+		"Hi %v, great to see you!",
+		"%v, hi! how you doing?",
+		"I am glad to see you, %v!",
+		"Hello, %v, nice to meet you!"
+	}
+	sentence := sentences[rand.Intn(len(sentences))]
+	return sentence
 }
