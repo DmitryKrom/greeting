af0734a6dcb249c159953222acbdd865354509a7
DmitryKrom <krom77@hotmail.de>
2021-07-13 22:07:12 +0200

    some chsnge

--- greeting.go
+++ greeting.go
@@ -6,10 +6,10 @@ import (
 )
 
 func Hello(name string) (string, error) {
-	m := fmt.Sprintf("Hi, %v, how are you doing?", name)
 	if name == "" {
 		return "", errors.New("no name given")
 	}
+	m := fmt.Sprintf("Hi, %v, how are you doing?", name)
 
 	return m, nil
 }
